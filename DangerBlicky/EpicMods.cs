6f458d951ed6b174371702cdc2c5df65b938817e
EpicRangeTime <48403096+EpicRangeTime@users.noreply.github.com>
2025-12-28 23:48:58 -0700

    Initial Commit

---
+++ DangerBlicky/EpicMods.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Spt.Mod;
+using Range = SemanticVersioning.Range;
+
+namespace DangerBlicky;
+
+public record ModMetadata : AbstractModMetadata
+{
+    public override string ModGuid { get; init; } = "com.epicrangetime.blicky";
+    public override string Name { get; init; } = "Danger Blicky";
+    public override string Author { get; init; } = "GrooveypenguinX, EpicRangeTime";
+    public override List<string>? Contributors { get; init; } = null;
+    public override SemanticVersioning.Version Version { get; init; } = new(typeof(ModMetadata).Assembly.GetName().Version?.ToString(3));
+    public override Range SptVersion { get; init; } = new("~4.0.3");
+    public override List<string>? Incompatibilities { get; init; }
+    public override Dictionary<string, Range>? ModDependencies { get; init; } = new()
+    {
+        { "com.wtt.commonlib", new Range("~2.0.9") }
+    };
+    public override string? Url { get; init; }
+    public override bool? IsBundleMod { get; init; } = true;
+    public override string License { get; init; } = "MIT";
+}
+
+[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 3)]
+public class DangerBlicky(
+    WTTServerCommonLib.WTTServerCommonLib wttCommon) : IOnLoad
+{
+    public async Task OnLoad()
+    {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        await wttCommon.CustomItemServiceExtended.CreateCustomItems(assembly);
+    }
+}
